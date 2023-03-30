8d11502599f7672f2b5474d9eea85d8f341e4a0b
omerdaube <102986147+omerdaube@users.noreply.github.com>
2023-03-30 16:49:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,25 @@
 import streamlit as st
-import pandas as pd
 
-st.write(1234)
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-}))
+def toggle_checkbox(checkbox_id):
+    checkboxes[checkbox_id][1] = not checkboxes[checkbox_id][1]
 
+with open("input.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()
+
+checkboxes = {}
+for i, line in enumerate(lines):
+    text, marked = line.strip().split(",")
+    checkboxes[i] = [text, marked == "YES"]
+
+st.set_page_config(page_title="helo", page_icon=":clipboard:")
+st.title("helo")
+
+for i in range(len(checkboxes)):
+    checkbox_id = str(i)
+    text, marked = checkboxes[i]
+    if marked:
+        checkbox_state = st.checkbox(text, key=checkbox_id)
+    else:
+        checkbox_state = st.checkbox(text, value=False, key=checkbox_id)
+    if checkbox_state:
+        toggle_checkbox(i)
