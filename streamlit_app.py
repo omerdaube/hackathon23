dcad1706437fdd38e313e9e8928cbe83bc01a9b3
omerdaube <102986147+omerdaube@users.noreply.github.com>
2023-03-30 16:50:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,24 @@ import streamlit as st
 def toggle_checkbox(checkbox_id):
     checkboxes[checkbox_id][1] = not checkboxes[checkbox_id][1]
 
-with open("input.txt", "r", encoding="utf-8") as file:
-    lines = file.readlines()
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    lines = uploaded_file.read().decode("utf-8").splitlines()
 
-checkboxes = {}
-for i, line in enumerate(lines):
-    text, marked = line.strip().split(",")
-    checkboxes[i] = [text, marked == "YES"]
+    checkboxes = {}
+    for i, line in enumerate(lines):
+        text, marked = line.strip().split(",")
+        checkboxes[i] = [text, marked == "YES"]
 
-st.set_page_config(page_title="helo", page_icon=":clipboard:")
-st.title("helo")
+    st.set_page_config(page_title="helo", page_icon=":clipboard:")
+    st.title("helo")
 
-for i in range(len(checkboxes)):
-    checkbox_id = str(i)
-    text, marked = checkboxes[i]
-    if marked:
-        checkbox_state = st.checkbox(text, key=checkbox_id)
-    else:
-        checkbox_state = st.checkbox(text, value=False, key=checkbox_id)
-    if checkbox_state:
-        toggle_checkbox(i)
+    for i in range(len(checkboxes)):
+        checkbox_id = str(i)
+        text, marked = checkboxes[i]
+        if marked:
+            checkbox_state = st.checkbox(text, key=checkbox_id)
+        else:
+            checkbox_state = st.checkbox(text, value=False, key=checkbox_id)
+        if checkbox_state:
+            toggle_checkbox(i)
